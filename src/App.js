95bb9ca087f41a8174ee857863de3b7b7f5cb678
callbacc <alexa.matei@icloud.com>
2020-10-24 19:19:50 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
 import React, { Component } from "react";
+import { CssBaseline, Typography, Box, AppBar, createMuiTheme, ThemeProvider, Grid, Link } from "@material-ui/core";
 
+function App() {
 
-export class App extends Component {
-    render() {
-        return (
-           <p>Hello from React!</p>
-        );
-    }
+    const theme = createMuiTheme({
+        palette: {
+            background: {
+                default: "#000000"
+            },
+            typography: {
+                fontFamily: 'Trispace',
+            },
+            primary: {
+                main: ""
+            },
+        },
+    });
+    const appName = "artistify"
+
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Box style={{
+                backgroundColor: "#23272a",
+                color: "white",
+            }}>
+                <AppBar>
+                    <Grid container>
+                        <Grid item>
+                            <Typography variant="h5">{appName}</Typography>
+                        </Grid>
+                        <Grid item >
+                            <Link>link 1</Link>
+                        </Grid>
+                        <Grid item >
+                            <Link>link 2</Link>
+                        </Grid>
+                        <Grid item >
+                            <Link>link 3</Link>
+                        </Grid>
+                        <Grid item >
+                            <Link>link 4</Link>
+                        </Grid>
+                    </Grid>
+                </AppBar>
+            </Box>
+        </ThemeProvider>
+    );
+
 }
 
 export default App;
