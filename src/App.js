a2f5f0b802cd6cd2cc4bcd7be5b0497055690de8
callbacc <alexa.matei@icloud.com>
2020-10-25 19:13:42 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
 import React, { Component } from "react";
-import { CssBaseline, Typography, Box, AppBar, createMuiTheme, ThemeProvider, Grid, Link } from "@material-ui/core";
+import {
+    CssBaseline,
+    Container,
+    Typography,
+    Box,
+    AppBar,
+    createMuiTheme,
+    ThemeProvider,
+    Grid,
+    Button,
+    Link
+}
+    from "@material-ui/core";
 
 function App() {
 
     const theme = createMuiTheme({
         palette: {
-            background: {
-                default: "#000000"
-            },
-            typography: {
-                fontFamily: 'Trispace',
-            },
-            primary: {
-                main: ""
-            },
+            type: "dark"
         },
     });
-    const appName = "artistify"
+
 
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
-            <Box style={{
-                backgroundColor: "#23272a",
-                color: "white",
-            }}>
-                <AppBar>
+            <AppBar position="static">
+                <Box p={2}>
                     <Grid container>
                         <Grid item>
-                            <Typography variant="h5">{appName}</Typography>
+                            <Typography style={{ fontFamily: "Trispace" }} variant="h5">artistify</Typography>
                         </Grid>
                         <Grid item >
                             <Link>link 1</Link>
@@ -43,8 +44,38 @@ function App() {
                             <Link>link 4</Link>
                         </Grid>
                     </Grid>
-                </AppBar>
-            </Box>
+                </Box>
+            </AppBar>
+            <Container>
+                <Box p={6}>
+
+                    <Grid container>
+                        <Grid sm={6} item container direction="column" spacing={3}>
+                            <Grid item>
+                                <Typography style={{ fontFamily: "Trispace" }} variant="h3">
+                                    Free Music, for Free
+                        </Typography>
+                            </Grid>
+                            <Grid item>
+                                <Typography style={{ fontFamily: "Trispace" }}>
+                                    Less ads, more free music. The way free music should be. Listen to any song available on multiple platforms including Youtube, with less ads, and less bullshit.
+                                </Typography>
+                            </Grid>
+                            <Grid item>
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    style={{ fontFamily: "Trispace" }}
+                                    href="https://artistify-2.appspot.com"
+                                > Open Artistify in your Browser</Button>
+                            </Grid>
+                        </Grid>
+                        <Grid sm={6} item>
+                            <img width="100%" src="sample.png"></img>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Container>
         </ThemeProvider>
     );
 
